b9c9636d74f4b487ffcd387f45c5ad2f06d1a3d9
apple <>
2021-01-04 09:58:51 +0800

    First commit

---
+++ LifeLiver/LifeLiver/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LifeLiver
+//
+//  Created by apple on 2021/1/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LifeLiver/LifeLiver/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LifeLiver
+//
+//  Created by apple on 2021/1/4.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
